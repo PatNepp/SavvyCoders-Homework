c99741c35b53f0820a283fe233ca438201a0b403
Patrick Nepp <ptnepp@gmail.com>
2020-06-24 22:12:01 -0500

    Adding homework

---
+++ Week 2.js
@@ -0,0 +1,10 @@
+let pizzaPlace = "Patrick's Pizza"
+let numberOfToppings = 10
+console.log(pizzaPlace, typeof pizzaPlace, numberOfToppings, typeof numberOfToppings);
+console.log(`${pizzaPlace} has the best damn pizza in town!`)
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.")
+} else {
+  console.log("A whole lot of pizza.")
+}
+
